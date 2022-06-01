15137baa7efa3aae9b93871cfc1eff1118505203
David Abol Ger <draker74@gmail.com>
2022-06-01 11:24:43 +0300

    todo with react

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,8 +3,7 @@ import { RiCloseCircleLine } from 'react-icons/ri';
 import { TiEdit } from 'react-icons/ti';
 import TodoForm from './TodoForm';
 
-// eslint-disable-next-line react/prop-types
-function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
+export default function Todo(todos, completeTodo, removeTodo, updateTodo) {
   const [edit, setEdit] = useState({
     id: null,
     value: '',
@@ -22,10 +21,12 @@ function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
     return <TodoForm edit={edit} onSubmit={submitUpdate} />;
   }
 
-  // eslint-disable-next-line react/prop-types
+  // eslint-disable-next-line react/destructuring-assignment
   return todos.map((todo, index) => (
     // eslint-disable-next-line react/no-array-index-key
     <div className={todo.isComplete ? 'todo-row complete' : 'todo-row'} key={index}>
+      {/* eslint-disable-next-line max-len */}
+      {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */}
       <div key={todo.id} onClick={() => completeTodo(todo.id)}>
         {todo.text}
       </div>
@@ -38,4 +39,3 @@ function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
     </div>
   ));
 }
-export default Todo;
